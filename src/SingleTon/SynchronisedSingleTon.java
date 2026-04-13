9f526fee53d83a051378dda7033d8ed46c6f87b9
tek.ninja <ninjatekpro@gmail.com>
2026-04-13 16:25:51 +0530

    Added comment for Singleton - Synchronized

--- src/SingleTon/SynchronisedSingleTon.java
+++ src/SingleTon/SynchronisedSingleTon.java
@@ -9,6 +9,12 @@ public class SynchronisedSingleTon {
     public static SynchronisedSingleTon getInstance(){
         if(instance == null){
             synchronized (SynchronisedSingleTon.class){
+                /*
+                 If some threads say 5 threads passed the first check, then
+                 after one thread exited the synchronized block, then it let the other
+                 threads enter the synchronized block, so, this second check prevents
+                 the other threads create another instance.
+                 */
                 if(instance == null ){
                     instance = new SynchronisedSingleTon();
                 }
